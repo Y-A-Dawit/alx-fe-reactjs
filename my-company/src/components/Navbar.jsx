d9d26f8d3fc9168a1526e5779af7c044891690f1
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-02-27 18:25:19 +0300

    update

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', borderBottom: '1px solid #ccc' }}>
+    <nav style={{ backgroundColor: '#333', display: 'flex',justifyContent:'center',padding: '10px', borderBottom: '1px solid #ccc',}}>
       <Link to="/" style={{ margin: '0 10px' }}>Home</Link>
       <Link to="/about" style={{ margin: '0 10px' }}>About</Link>
       <Link to="/services" style={{ margin: '0 10px' }}>Services</Link>
