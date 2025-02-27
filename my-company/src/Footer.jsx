56a0d92a4c0bb2eef812a17b92be89ae4bd54093
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-02-27 18:08:32 +0300

    update

---
+++ my-company/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+      <footer style={{ textAlign: 'center', padding: '10px', borderTop: '1px solid #ccc', marginTop: '20px' }}>
+        <p>&copy; 2025 My Company. All rights reserved.</p>
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
