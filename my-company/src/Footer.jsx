759ae4a00f823a89c8a3c296e2a35503d838259d
Yohannes <yohannesabreham08@gmail.com>
2025-02-27 17:40:31 +0300

    Delete my-company directory

--- my-company/src/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-      <footer style={{ textAlign: 'center', padding: '10px', borderTop: '1px solid #ccc', marginTop: '20px' }}>
-        <p>&copy; 2025 My Company. All rights reserved.</p>
-      </footer>
-    );
-  }
-  
-  export default Footer;
