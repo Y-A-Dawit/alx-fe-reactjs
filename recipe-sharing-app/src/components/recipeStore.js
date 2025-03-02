a5e30742253dfa3d3a8a6bbe02d9223ac9a38d8f
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-03-02 16:22:37 +0300

    update

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,25 +1,91 @@
-import { create } from 'zustand';
+import create from 'zustand';
 
 const useRecipeStore = create((set) => ({
   recipes: [],
+  searchTerm: '', // <-- Added search term state
 
-  // Add a new recipe
   addRecipe: (newRecipe) =>
     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
 
-  // Delete a recipe
-  deleteRecipe: (id) =>
+  deleteRecipe: (recipeId) =>
     set((state) => ({
-      recipes: state.recipes.filter((recipe) => recipe.id !== id),
+      recipes: state.recipes.filter((recipe) => recipe.id !== recipeId),
     })),
 
-  // Update a recipe
-  updateRecipe: (id, updatedRecipe) =>
+  updateRecipe: (updatedRecipe) =>
     set((state) => ({
       recipes: state.recipes.map((recipe) =>
-        recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+        recipe.id === updatedRecipe.id ? updatedRecipe : recipe
       ),
     })),
+
+  setSearchTerm: (term) => set({ searchTerm: term }), // <-- Added function to update search term
 }));
 
 export default useRecipeStore;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { create } from 'zustand';
+
+// const useRecipeStore = create((set) => ({
+//   recipes: [],
+
+//   // Add a new recipe
+//   addRecipe: (newRecipe) =>
+//     set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+
+//   // Delete a recipe
+//   deleteRecipe: (id) =>
+//     set((state) => ({
+//       recipes: state.recipes.filter((recipe) => recipe.id !== id),
+//     })),
+
+//   // Update a recipe
+//   updateRecipe: (id, updatedRecipe) =>
+//     set((state) => ({
+//       recipes: state.recipes.map((recipe) =>
+//         recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
+//       ),
+//     })),
+// }));
+
+// export default useRecipeStore;
