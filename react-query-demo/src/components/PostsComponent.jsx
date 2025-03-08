89f7022291b3545e3aceabb30bda3d5462ecf4b9
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-03-08 16:39:12 +0300

    update

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -9,9 +9,22 @@ const fetchPosts = async () => {
 };
 
 function PostsComponent() {
-  const { data, error, isLoading, isError, refetch } = useQuery(
-    "posts",
-    fetchPosts
+  const {
+    data,
+    error,
+    isLoading,
+    isError,
+    refetch,
+    isFetching,
+  } = useQuery(
+    "posts", 
+    fetchPosts, 
+    {
+      cacheTime: 1000 * 60 * 5, // Cache the data for 5 minutes
+      staleTime: 1000 * 60, // Consider the data fresh for 1 minute
+      refetchOnWindowFocus: true, // Refetch data when window regains focus
+      keepPreviousData: true, // Keep previous data while loading new data
+    }
   );
 
   // Handle loading state
@@ -24,6 +37,7 @@ function PostsComponent() {
     <div>
       <h2>Posts</h2>
       <button onClick={() => refetch()}>Refetch Posts</button>
+      {isFetching && <p>Refreshing data...</p>}
       <ul>
         {data.map((post) => (
           <li key={post.id}>{post.title}</li>
