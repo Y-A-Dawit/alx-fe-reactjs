b89b57fb6d0484c90d02aa0e5f6e0b6d17ec77f9
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-03-16 22:41:40 +0300

    update

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 function UserProfile() {
     return (
-      <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-md mx-auto my-20 rounded-lg shadow-lg text-center">
+      <div className="bg-gray-100 p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-sm md:max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
         <img
           src="https://via.placeholder.com/150"
           alt="User"
-          className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 rounded-full mx-auto sm:p-4"
+          className="w-24 h-24 sm:w-32 sm:h-32 md:w-36 md:h-36 rounded-full mx-auto sm:p-4 md:max-w-sm"
         />
         <h1 className="text-lg sm:text-xl md:text-2xl text-blue-800 my-4">John Doe</h1>
         <p className="text-sm sm:text-base text-gray-600">
