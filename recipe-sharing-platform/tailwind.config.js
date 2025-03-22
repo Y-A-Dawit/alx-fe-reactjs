65830d97b7e7017304164070cd8d10ef031978e4
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-03-22 14:18:07 +0300

    update

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'];
+export const darkMode = false;
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
