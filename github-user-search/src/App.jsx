e4aea167f206df58a9d63ad5fcc90548474f8c03
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-03-27 15:36:31 +0300

    update

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,14 +1,19 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
+import SearchBar from './components/SearchBar';
 import './App.css'
 
 function App() {
+  const handleSearch = (username) => {
+    console.log("Searching for:", username)
+  }
 
   return (
     <>
       <div>
         <h1>GitHub User Search</h1>
+        <SearchBar onSearch={handleSearch} />
         <p>Start searching for GitHub users!</p>
       </div>
     </>
