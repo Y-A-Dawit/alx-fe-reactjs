f575a0bc2be7b836443e1a6d872fcb95b83702d7
Yohannes Dawit <yohannesabreham08@gmail.com>
2025-03-27 17:17:13 +0300

    update

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,26 +1,26 @@
 import axios from 'axios';
 
 // GitHub Search API URL
-const API_URL = "https://api.github.com/search/users?q";
+const API_URL = "https://api.github.com/search/users";
+const apiKey = import.meta.env.VITE_APP_GITHUB_API_KEY;
 
 export const fetchUserData = async (username, location = "", minRepos = "") => {
   try {
     // Construct the query string based on the user's inputs
     let query = `q=${username}`;
     
-    // Add location to query if it's provided
     if (location) query += `+location:${location}`;
-    
-    // Add minimum repositories to query if it's provided
     if (minRepos) query += `+repos:>=${minRepos}`;
 
     // Make the API request with the constructed query string
-    const response = await axios.get(`${API_URL}?${query}`);
-    
-    // Return the list of users matching the search criteria
+    const response = await axios.get(`${API_URL}?${query}`, {
+      headers: {
+        Authorization: `token ${apiKey}` // Include API key in the headers
+      }
+    });
+
     return response.data.items;
   } catch {
-    // If there's an error, throw it for further handling
     throw new Error('User not found or query error');
   }
 };
